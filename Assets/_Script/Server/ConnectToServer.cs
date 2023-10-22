e08471a4e5afdd26e190d681b0b142e56c70927c
phongtruong2106 <91039342+phongtruong2106@users.noreply.github.com>
2023-10-22 22:27:06 +0700

    Update Photon

--- Assets/_Script/Server/ConnectToServer.cs
+++ Assets/_Script/Server/ConnectToServer.cs
@@ -7,21 +7,12 @@ using UnityEngine.UI;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
-    public InputField usernameInput;
-    public Text button;
-
-    public void OnClickConnect()
-    {
-        if(usernameInput.text.Length >= 1)
-        {
-            PhotonNetwork.NickName = usernameInput.text;
-            button.text = "Connecting.....";
-            PhotonNetwork.AutomaticallySyncScene = true;
-            PhotonNetwork.ConnectUsingSettings();
-        }
+    private void Start() {
+        PhotonNetwork.ConnectUsingSettings();
     }
+
     public override void OnConnectedToMaster()
     {
-        SceneManager.LoadScene("Lobby");
+        SceneManager.LoadScene("MainMenu");
     }
 }
---
+++ Assets/_Script/Server/MainMenu.cs
@@ -0,0 +1,27 @@
+
+using TMPro;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class MainMenu : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField createInput;
+    public TMP_InputField joinInput;
+
+    public void CreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        PhotonNetwork.CreateRoom(createInput.text, roomOptions);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+}
